5d438d6cb5dd4a0b03b6d8fd1b188d400484b3a6
Kriter, John <JKriter@carnival.com>
2017-05-16 13:42:35 -0500

    initial commit

--- tutorial1.groovy
+++ tutorial1.groovy
@@ -1,21 +1,11 @@
 #!groovy
+
 pipeline {
     agent any
-
     stages {
         stage('Build') {
             steps {
                 echo 'Building..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
             }
         }
     }
