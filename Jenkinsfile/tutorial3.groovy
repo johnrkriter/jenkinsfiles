3536ee80b770c2d77e265415859cd99e2de7373e
Kriter, John <JKriter@carnival.com>
2017-05-16 14:17:17 -0500

    Initial develop commit

---
+++ Jenkinsfile/tutorial3.groovy
@@ -0,0 +1,22 @@
+#!groovy
+
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..3'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..3'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....3'
+            }
+        }
+    }
+}
